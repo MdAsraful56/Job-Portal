27c781aafcce4a9a6bb4ed383364cb83a583fe82
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-27 07:01:08 +0600

    folder str & react router setup

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+const MainLayout = () => {
+    return (
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+            <Outlet/>
+        </div>
+    );
+};
+
+export default MainLayout;
